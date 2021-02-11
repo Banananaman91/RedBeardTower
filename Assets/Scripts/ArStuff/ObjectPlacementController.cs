96aeeab524610c9442e8fbb4d19569104065f59b
Enrico <sandri.enrico86@gmail.com>
2021-02-11 14:14:11 +0000

    Obj instantiation
    Implementation of raycast from input screen position to detected plane with instantiation of prefab onto selected plane. SET TO HORIZONTAL PLANES ONLY FOR THE PURPOSE OF THE GAME IN PLANE MANAGER

---
+++ Assets/Scripts/ArStuff/ObjectPlacementController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+
+public class ObjectPlacementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placedPrefab;
+
+    public GameObject PlacedPrefab { get; set; }
+
+    private ARRaycastManager aRRaycastManager;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition)) return;
+
+        if (aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinBounds))
+        {
+            var hitPose = hits[0].pose;
+
+            Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+        }
+    }
+}
