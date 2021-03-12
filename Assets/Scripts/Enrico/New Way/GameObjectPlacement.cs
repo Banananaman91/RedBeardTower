fd72c5a49d83818f048b997bd10093f2ce1fd77d
Enrico <sandri.enrico86@gmail.com>
2021-03-12 13:41:12 +0000

    raycast with reticle to place towers

--- Assets/Scripts/Enrico/New Way/GameObjectPlacement.cs
+++ Assets/Scripts/Enrico/New Way/GameObjectPlacement.cs
@@ -2,55 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Android;
 
 public class GameObjectPlacement : MonoBehaviour
 {
     [SerializeField]
     private Button placeTowerButton;
+    public Text debugText;
+    public Image reticleUI;
+    private Touch theTouch;
 
     public GameObject placedGameObject;
     private  Vector3 touchPositon;
 
     private void Awake()
     {
-        //placeTowerButton.onClick.AddListener(()=> changePrefabToPlace("Tower"));
+        reticleUI.gameObject.SetActive(false);
     }
-
-    //private void changePrefabToPlace(string prefabName)
-    //{
-    //    placedGameObject
-    //    //placedGameObject = Resources.Load<GameObject>($"Prefabs/Towers/{prefabName}");
-
-    //    if(placedGameObject == null)
-    //    {
-    //        Debug.Log("Check prefab path and name");
-    //    }
-    //}
-
-    //bool TryGetTOuchPosition(out Vector2 touchPositon)
-    //{
-    //    if(Input.touchCount > 0)
-    //    {
-    //        touchPositon = Input.GetTouch(0).position;
-    //    }
-    //    touchPositon = default;
-    //    return false;
-    //}
-
-    private void Update()
+   
+    public void CameraRaycast()
     {
-        //if(!TryGetTOuchPosition( out Vector2 touchPositon))
-        //{
-        //    return;
-        //}
-        if (Input.touchCount > 0)
+        
+        reticleUI.gameObject.SetActive(true);
+        if (Input.touchCount > 0 )
+        {
+            theTouch = Input.GetTouch(0);
+            if(theTouch.phase == TouchPhase.Ended)
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+                {
+                    switch (hit.collider.name)
+                    {
+                        default:
+                            break;
+
+                        case "PlayArea(Clone)":
+                            debugText.text = "hit THE FKING PLANE";
+                            reticleUI.color = Color.red;
+                            Instantiate(placedGameObject, hit.point, Quaternion.identity);
+                            break;
+                    }
+                }
+                else
+                {
+                    reticleUI.color = Color.white;
+                }
+            }
+        }
+    }
+    private void TouchPositioRaycast()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             touchPositon = Input.GetTouch(0).position;
-            RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(touchPositon);
-            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                Instantiate(placedGameObject, hit.point, transform.rotation);
+                switch (hit.collider.name)
+                {
+                    default:
+                        break;
+
+                    case "PlayArea(Clone)":
+                        debugText.text = "hit THE FKING PLANE";
+                        break;
+                }
+                //Instantiate(placedGameObject, hit.point, transform.rotation);
+            }
+        }
+    }
+
+    private void MousePositionRaycast()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //touchPositon = Input.GetTouch(0).position;    
+            // Ray ray = Camera.main.ScreenPointToRay(touchPositon);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                switch (hit.collider.name)
+                {
+                    default:
+                        break;
+
+                    case "PlayArea": debugText.text = "hit THE FKING PLANE"; break;
+                }
+                // Instantiate(placedGameObject, hit.point, transform.rotation);
             }
         }
     }
